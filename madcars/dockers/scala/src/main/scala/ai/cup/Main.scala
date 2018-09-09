ec37048c4c2aa3d1922c4d9441f3b9183e32090c
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-09-09 22:42:34 +0300

    random moves

--- madcars/dockers/scala/src/main/scala/ai/cup/Main.scala
+++ madcars/dockers/scala/src/main/scala/ai/cup/Main.scala
@@ -1,40 +1,31 @@
 package ai.cup
 
-import com.rojoma.json.v3.ast.{JNumber, JString, JValue}
+import com.rojoma.json.v3.ast.JValue
 import com.rojoma.json.v3.util.JsonUtil
 
+import scala.util.Random
+
 object Main {
-	def onTick(parsed : Map[String, Array[Map[String, JValue]]]) : Map[String, JValue] = {
-
-		if (parsed.get("Mine").isDefined) {
-
-			val foods = findFood(parsed("Objects"))
-
-			if (foods.length > 0) {
-				Map("X" -> foods.head("X").cast[JNumber].get, "Y" -> foods.head("Y").cast[JNumber].get)
-			} else {
-				Map("X" -> JNumber(300), "Y" -> JNumber(300), "Debug" -> JString("No food"))
-			}
-
-		} else {
-			Map("X" -> JNumber(0), "Y" -> JNumber(0), "Debug" -> JString("Died"))
-		}
+	def onTick(parsed : Map[String, JValue]) : (String, String) = {
+		val commands = Seq("left", "right", "stop")
+		val command = new Random().shuffle(commands).head
+		(command, command)
 	}
 
-	def findFood(objects : Array[Map[String, JValue]]) : Array[Map[String, JValue]] = {
-		objects.filter(t => t("T") == JString("F"))
-	}
+	def init(parsed : Map[String, JValue]): Unit = {}
 
 	def main(args : Array[String]) {
 		val configStr = scala.io.StdIn.readLine().trim
-		val config = JsonUtil.parseJson[Map[String, JValue]](configStr)
+		val config = JsonUtil.parseJson[Map[String, JValue]](configStr).right.get
 
-		for(tick <- 0 to config.right.get("GAME_TICKS").cast[JNumber].get.toInt) {
+		init(config)
+
+		while (true) {
 			val json = scala.io.StdIn.readLine().trim
-			val data = JsonUtil.parseJson[Map[String, Array[Map[String, JValue]]]](json)
+			val data = JsonUtil.parseJson[Map[String, JValue]](json)
 
-			val response = onTick(data.right.get)
-			println(JsonUtil.renderJson(response))
+			val (command, debug) = onTick(data.right.get)
+			println(JsonUtil.renderJson(Map("command" -> command, "debug" -> debug)))
 		}
 	}
 }
